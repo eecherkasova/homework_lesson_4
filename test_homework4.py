f65a3a2ac0b29240324ea5e054e03d364bf8d792
Elizaveta <eecherkasova@mail.ru>
2023-11-17 17:20:47 +0300

    hw_4_upd: add .gitignore, requirements.txt, add outputs in some tests

--- test_homework4.py
+++ test_homework4.py
@@ -1,5 +1,4 @@
 import math
-import random
 from random import randint
 
 
@@ -7,39 +6,45 @@ def test_greeting():
     name = "Анна"
     age = 25
     output = f"Привет, {name}! Тебе {age} лет."
+    print(output)
     assert output == "Привет, Анна! Тебе 25 лет."
+
 
 def test_rectangle():
     a = 10
     b = 20
-    perimeter = a*2 + b*2
+    perimeter = a * 2 + b * 2
     assert perimeter == 60
-    area = a*b
+    area = a * b
     assert area == 200
+
 
 def test_circle():
     r = 23
-    area = math.pi*(r**2)
+    area = math.pi * (r ** 2)
     assert area == 1661.9025137490005
-    length = 2*math.pi*r
+    length = 2 * math.pi * r
     assert length == 144.51326206513048
+    print(area)
+    print(length)
 
 def test_random_list():
     l = [
-    randint(1,101),
-    randint(1,101),
-    randint(1,101),
-    randint(1,101),
-    randint(1,101),
-    randint(1,101),
-    randint(1,101),
-    randint(1,101),
-    randint(1,101),
-    randint(1,101)
+        randint(1, 101),
+        randint(1, 101),
+        randint(1, 101),
+        randint(1, 101),
+        randint(1, 101),
+        randint(1, 101),
+        randint(1, 101),
+        randint(1, 101),
+        randint(1, 101),
+        randint(1, 101)
     ]
     l.sort()
     assert len(l) == 10
     assert l[0] < l[-1]
+
 
 def test_unique_elements():
     l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
@@ -48,9 +53,11 @@ def test_unique_elements():
     assert len(l) == 10
     assert l == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+
 def test_dicts():
     first = ["a", "b", "c", "d", "e"]
     second = [1, 2, 3, 4, 5]
     d = dict(zip(first, second))
+    print(d)
     assert isinstance(d, dict)
     assert len(d) == 5
